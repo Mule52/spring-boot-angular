60d97522f8f431d3fefe7c4eac08a92ff5d87bdb
Alex Mueller <amueller@signingday.com>
2015-11-15 13:29:32 -0700

    updating angular authorization

--- src/main/resources/static/js/errorController.js
+++ src/main/resources/static/js/errorController.js
@@ -2,6 +2,7 @@ angular.module('demo')
     .controller('errorController', ['$scope', '$http',
         function($scope, $http) {
 
+            // TODO: this is received 403 only at this time, so update it later.
             console.log("errorController called");
         }])
 ;
